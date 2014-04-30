6ed23de79350c9a8ca12ec4413d73e89d5f4d111
mypikachu <my_pikachu@web.de>
2014-04-30 08:58:49 -0700

    FBEditor Version 0.7.0.5
    FBEditor Version 0.7.0.5
    
    NetBeans 8 und Java 1.7

--- src/de/FBEditor/utils/Listener.java
+++ src/de/FBEditor/utils/Listener.java
@@ -1,5 +1,9 @@
 package de.FBEditor.utils;
 
+import de.FBEditor.CutAndPastePopup;
+import de.FBEditor.FBEdit;
+import de.FBEditor.FindReplace;
+import de.FBEditor.struct.JTextPane2;
 import java.awt.Component;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -7,16 +11,10 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-
 import javax.swing.JPopupMenu;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.JTextComponent;
-
-import de.FBEditor.CutAndPastePopup;
-import de.FBEditor.FBEdit;
-import de.FBEditor.FindReplace;
-import de.FBEditor.struct.JTextPane2;
 
 public class Listener {
 
@@ -25,12 +23,15 @@ public class Listener {
 		
 		pane2.addKeyListener(new KeyListener() {
 
+                        @Override
 			public void keyTyped(KeyEvent keyevent) {
 			}
 
+                        @Override
 			public void keyPressed(KeyEvent keyevent) {
 			}
 
+                        @Override
 			public void keyReleased(KeyEvent arg0) {
 				FindReplace findReplace = fbedit.getFindReplace();
 				int key = arg0.getKeyCode();
@@ -53,25 +54,32 @@ public class Listener {
 	public static void addWinListener(final FBEdit fbedit) {
 		fbedit.addWindowListener(new WindowListener() {
 
+                        @Override
 			public void windowOpened(WindowEvent windowevent) {
 			}
 
+                        @Override
 			public void windowClosing(WindowEvent e) {
 				fbedit.exit();
 			}
 
+                        @Override
 			public void windowClosed(WindowEvent windowevent) {
 			}
 
+                        @Override
 			public void windowIconified(WindowEvent windowevent) {
 			}
 
+                        @Override
 			public void windowDeiconified(WindowEvent windowevent) {
 			}
 
+                        @Override
 			public void windowActivated(WindowEvent windowevent) {
 			}
 
+                        @Override
 			public void windowDeactivated(WindowEvent windowevent) {
 			}
 		});
@@ -80,13 +88,16 @@ public class Listener {
 	public static DocumentListener myDocumentListener(final FBEdit fbedit) {
 		DocumentListener myDocumentListener = new DocumentListener() {
 
+                        @Override
 			public void insertUpdate(DocumentEvent aEvent) {
 				fbedit.updateMenu(fbedit.getMenu());
 			}
 
+                        @Override
 			public void removeUpdate(DocumentEvent documentevent) {
 			}
 
+                        @Override
 			public void changedUpdate(DocumentEvent aEvent) {
 				fbedit.updateMenu(fbedit.getMenu());
 			}
@@ -99,6 +110,7 @@ public class Listener {
 	
 	fbedit.getJTextPane().addMouseListener(new MouseAdapter() {
 
+                @Override
 		public void mousePressed(MouseEvent e) {
 			if (e.isPopupTrigger()) {
 				cutAndPastePopup.updateSource((JTextComponent) e.getSource());
@@ -107,6 +119,7 @@ public class Listener {
 			}
 		}
 
+                @Override
 		public void mouseReleased(MouseEvent e) {
 			if (e.isPopupTrigger()) {
 				cutAndPastePopup.updateSource((JTextComponent) e.getSource());
